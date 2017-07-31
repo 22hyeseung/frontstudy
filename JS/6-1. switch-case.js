4e4050bee660256ba743b530d1229236b0fc92ad
huusz <irinso93@gmail.com>
2017-07-31 20:57:22 +0900

    update JS

---
+++ JS/6-1. switch-case.js
@@ -0,0 +1,15 @@
+var color = 'red';
+
+switch (color) {
+  case 'yellow':
+    console.log('yellow color');
+    break;
+  case 'red':
+    console.log('red color');
+    break;
+  case 'blue':
+    console.log('blue color');
+    break;
+  default:
+    console.log('unknown color');
+}
