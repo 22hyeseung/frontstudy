585919a32ec3ae2e77cdbe3c8e4930ba893d0623
huusz <irinso93@gmail.com>
2017-08-19 23:45:36 +0900

    add clock on header

--- TodoList/todoinit/public/js/index.js
+++ TodoList/todoinit/public/js/index.js
@@ -1,19 +1,40 @@
 // vanilla
-// Header 오늘 날짜 가져오는 함수
-function getDateToday() {
-  var today = new Date();
-  var yyyy = today.getFullYear();
-  var mm = today.getMonth() + 1;
-  var dd = today.getDate();
-  var dayName = ['일요일', '월요일', '화요일', '수요알', '목요일', '금요일', '토요일'];
-  var day = dayName[today.getDay()];
 
-  // var yy = parseInt(yyyy).toString().slice(2, 4);
+(function (window, document) {
+  var date = new Date();
 
-  today = yyyy + '년 ' + mm + '월 ' + dd + '일 ' + day;
+  // 오늘 날짜 가져오는 함수
+  function getDateToday() {
+    var yyyy = date.getFullYear();
+    var mm = date.getMonth() + 1;
+    var dd = date.getDate();
+    var dayName = ['일요일', '월요일', '화요일', '수요알', '목요일', '금요일', '토요일'];
+    var day = dayName[date.getDay()];
 
-  return today;
-}
+    // var yy = parseInt(yyyy).toString().slice(2, 4);
+    return yyyy + '년 ' + mm + '월 ' + dd + '일 ' + day;
+  }
+
+  var today = document.getElementById('today');
+  today.append(getDateToday());
+
+  // 시계 - 실시간
+  // function getRealTime() {
+  //   var h = date.getHours();
+  //   var m = date.getMinutes();
+  //   var s = date.getSeconds();
+
+  //   if (h < 10) { h = '0' + h };
+  //   if (m < 10) { m = '0' + m };
+  //   if (s < 10) { s = '0' + s };
+
+  //   document.getElementById('clock').innerHTML = h + ':' + m + ':' + s;
+  //   setTimeout(getRealTime, 500);
+  // }
+
+})(window, document);
+
+
 
 // 진행률 로더
 moveProgressBar();
@@ -23,7 +44,6 @@ $(window).resize(function () {
 });
 
 function moveProgressBar() {
-  console.log("moveProgressBar");
   var getPercent = ($('.progress-wrap').data('progress-percent') / 100);
   var getProgressWrapWidth = $('.progress-wrap').width();
   var progressTotal = getPercent * getProgressWrapWidth;
@@ -36,11 +56,6 @@ function moveProgressBar() {
   }, animationLength);
 }
 
-// DOM
-(function (window, document) {
-  var today = document.getElementById('today');
-  today.append(getDateToday());
-})(window, document);
 
 // jQuery
 // Contents 할 일 입력란 액션
@@ -65,6 +80,6 @@ $(".button").mouseout(function () {
   $(this).css('color', '#eee');
 })
 
-$(".button").click(function () {
-  $(".todo-input-group").css('display', 'block');
-})
+// $(".button").click(function () {
+//   $(".todo-input-group").css('display', 'block');
+// })
