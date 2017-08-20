74ee85401bd5e2c4a34b987da7c8c76205113140
huusz <irinso93@gmail.com>
2017-08-20 17:03:23 +0900

    add button js

--- TodoList/todoinit/public/js/index.js
+++ TodoList/todoinit/public/js/index.js
@@ -1,5 +1,4 @@
-// vanilla
-
+// Header
 (function (window, document) {
   var date = new Date();
 
@@ -32,57 +31,56 @@
   //   setTimeout(getRealTime, 500);
   // }
 
+  // 진행률 로더
+  moveProgressBar();
+  //브라우저 크기 조절 시 다시 로드
+  $(window).resize(function () {
+    moveProgressBar();
+  });
+
+  function moveProgressBar() {
+    var getPercent = ($('.progress-wrap').data('progress-percent') / 100);
+    var getProgressWrapWidth = $('.progress-wrap').width();
+    var progressTotal = getPercent * getProgressWrapWidth;
+    var animationLength = 2500;
+
+    // on page load, animate percentage bar to data percentage length
+    // .stop() used to prevent animation queueing
+    $('.progress-bar').stop().animate({
+      left: progressTotal
+    }, animationLength);
+  }
+
 })(window, document);
 
 
+//Contents
+(function (window, document) {
 
-// 진행률 로더
-moveProgressBar();
-//브라우저 크기 조절 시 다시 로드
-$(window).resize(function () {
-  moveProgressBar();
-});
+  // 할 일 입력란 액션
+  $(".input").focus(function () {
+    $(this).parent().addClass("is-active is-completed");
+  });
 
-function moveProgressBar() {
-  var getPercent = ($('.progress-wrap').data('progress-percent') / 100);
-  var getProgressWrapWidth = $('.progress-wrap').width();
-  var progressTotal = getPercent * getProgressWrapWidth;
-  var animationLength = 2500;
+  $(".input").focusout(function () {
+    if ($(this).val() === "")
+      $(this).parent().removeClass("is-completed");
+    $(this).parent().removeClass("is-active");
+  })
 
-  // on page load, animate percentage bar to data percentage length
-  // .stop() used to prevent animation queueing
-  $('.progress-bar').stop().animate({
-    left: progressTotal
-  }, animationLength);
-}
+  // 할 일 추가 버튼
+  $(".button").mouseover(function () {
+    $(this).css('background-color', '#333');
+    $(this).css('color', '#ccadaf');
+  })
 
+  $(".button").mouseout(function () {
+    $(this).css('background-color', '#ccadaf');
+    $(this).css('color', '#eee');
+  })
 
-
-//
-
-// jQuery
-// Contents 할 일 입력란 액션
-$(".input").focus(function () {
-  $(this).parent().addClass("is-active is-completed");
-});
-
-$(".input").focusout(function () {
-  if ($(this).val() === "")
-    $(this).parent().removeClass("is-completed");
-  $(this).parent().removeClass("is-active");
-})
-
-// Contents 할 일 추가 버튼
-$(".button").mouseover(function () {
-  $(this).css('background-color', '#333');
-  $(this).css('color', '#ccadaf');
-})
-
-$(".button").mouseout(function () {
-  $(this).css('background-color', '#ccadaf');
-  $(this).css('color', '#eee');
-})
-
-$(".button").click(function () {
-  $(".todo-input-group").css('display', 'block');
-})
+  $(".button").click(function () {
+    $(".todo-input-group").css('display', 'block');
+    $(".button > i").attr('class', 'fa fa-times');
+  })
+})(window, document);
