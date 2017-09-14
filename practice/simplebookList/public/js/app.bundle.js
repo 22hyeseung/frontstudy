72c492551b2e44741edacdfb32d3ff1b57bbfac0
huusz <irinso93@gmail.com>
2017-09-14 16:34:04 +0900

    아이템 추가하기

--- practice/simplebookList/public/js/app.bundle.js
+++ practice/simplebookList/public/js/app.bundle.js
@@ -9027,16 +9027,28 @@ var _ajax2 = _interopRequireDefault(_ajax);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 // 테이블
 var tbody = document.querySelector('tbody');
 
 // 버튼
 var addBtn = document.querySelector('#add-btn');
-var editBtn = document.querySelector('#del-btn');
+// const editBtn = document.querySelector('#del-btn');
+
+var BookList = function BookList() {
+  _classCallCheck(this, BookList);
+
+  this.url = '/books';
+  this.books = [];
+};
 
 // =============================================
 // 함수
+
 // (1) bookList를 그려주는 기능
+
+
 function render(data) {
   // 리스트 초기화
   tbody.innerHTML = '';
@@ -9050,93 +9062,105 @@ function render(data) {
 function appendInput(id) {
   // const id = length + 1;
   // tbody의 마지막 엘리먼트로 추가한다.
-  tbody.insertAdjacentHTML('beforeend', '<tr>\n    <th>' + id + '</th>\n    <td><input class="validate" type="text" id="title" placeholder="\uCC45 \uC81C\uBAA9\uC744 \uC785\uB825\uD558\uC138\uC694."></td>\n    <td><input class="validate" type="text" id="author" placeholder="\uC800\uC790\uB97C \uC785\uB825\uD558\uC138\uC694."></td>\n    <td><input class="validate" type="text" id="price" placeholder="\uAC00\uACA9\uC744 \uC785\uB825\uD558\uC138\uC694."></td>\n    <td>  \n      <a class="waves-effect waves-gray btn-flat" id="submit-btn" data-item="' + id + '" data-type="submit">\n        <i class="medium material-icons">done</i>\n      </a>\n      <a class="waves-effect waves-gray btn-flat" id="cancel-btn" data-item="' + id + '" data-type="cancel">\n        <i class="medium material-icons">cancel</i>\n      </a>\n      <a class="waves-effect waves-gray btn-flat" id="del-btn" data-item="' + id + '" data-type="delete">\n        <i class="medium material-icons">delete</i>\n      </a>\n    </td>\n    </tr>');
+  tbody.insertAdjacentHTML('beforeend', '<tr>\n    <th>' + id + '</th>\n    <td><input class="validate" type="text" id="title" placeholder="\uCC45 \uC81C\uBAA9\uC744 \uC785\uB825\uD558\uC138\uC694."></td>\n    <td><input class="validate" type="text" id="author" placeholder="\uC800\uC790\uB97C \uC785\uB825\uD558\uC138\uC694."></td>\n    <td><input class="validate" type="text" id="price" placeholder="\uAC00\uACA9\uC744 \uC785\uB825\uD558\uC138\uC694."></td>\n    <td>  \n      <a class="waves-effect waves-gray btn-flat" id="submit-btn">\n        <i class="medium material-icons" data-item="' + id + '" data-type="submit">done</i>\n      </a>\n      <a class="waves-effect waves-gray btn-flat" id="cancel-btn">\n        <i class="medium material-icons" data-item="' + id + '" data-type="cancel">cancel</i>\n      </a>\n      <a class="waves-effect waves-gray btn-flat" id="del-btn">\n        <i class="medium material-icons"  data-item="' + id + '" data-type="delete">delete</i>\n      </a>\n    </td>\n    </tr>');
 }
 
 // (3) 새 아이템 추가 기능
-function addItem(inputData) {
-  // post해서 받은 데이터를 추가한다.
-  _ajax2.default.post('/books', inputData).then(function (data) {
-    render(JSON.parse(data));
-  });
+function addItem(newData) {
+  // console.log(newData);
+  // 아이템이 추가된 배열을 post로 보낸다.
+  _ajax2.default.post('/books', JSON.stringify(newData));
 }
 
 // (4) 기존 아이템 업데이트 기능
-function updateItem(inputData, id) {
-  var path = '/books' + id;
-  _ajax2.default.put(path, inputData).then(function (data) {
-    render(JSON.parse(data));
-  });
+function updateItem(newData, id) {
+  var path = '/books/' + id;
+  _ajax2.default.put(path, JSON.stringify(newData));
 }
 
 // =============================================
 // 화면
 // 1. 첫 페이지: 북리스트
-_ajax2.default.get('/books').then(function (data) {
-  // console.log(JSON.parse(data));
-  render(JSON.parse(data));
-});
 
-// 2. 리스트에 아이템(book) 추가
+// bookList 로드
+
+function loadList() {
+  _ajax2.default.get('/books').then(function (data) {
+    BookList.books = JSON.parse(data);
+    // console.log(BookList.books);
+    render(BookList.books);
+  });
+}
+
+loadList();
+
+// 2. add버튼 눌렀을 때
 addBtn.addEventListener('click', function () {
   // 현재 리스트 아이템 갯수 = tr의 갯수
   var length = tbody.childElementCount;
   var id = length + 1;
+
+  // 리스트에 input 항목 추가
   appendInput(id);
-});
-
-tbody.addEventListener('click', function (e) {
-  if (!e.target || e.target.nodeName !== 'BUTTON') return;
-  // 이벤트 걸어줄 대상이 되는 버튼과 데이터 ID
-  var tragetID = e.target.dataset.item * 1;
-
-  // 클릭 이벤트에서만 생기는 버튼
-  var submitBtn = document.querySelector('#submit-btn');
-  var cancelBtn = document.querySelector('#cancel-btn');
-
-  // 사용자가 입력한 input값
-  var title = document.querySelector('#title');
-  var author = document.querySelector('#author');
-  var price = document.querySelector('#price');
-
-  var inputData = { title: title, author: author, price: price };
-  inputData.editable = false;
 
   // 2-1. submit을 누르는 경우
-  submitBtn.addEventListener('click', function () {
-    // 첫 생성인 경우
-    if (!inputData.editable) {
-      inputData.status = 'new';
-      inputData.editable = true;
-      // post
-      addItem(inputData);
-    } else {
-      // 수정인 경우
-      inputData.status = 'edited';
-      // put
-      updateItem(inputData, tragetID);
+  // submitBtn.addEventListener('click', () => {
+  tbody.addEventListener('click', function (e) {
+    // 사용자가 입력한 input값
+    var title = document.querySelector('#title').value;
+    var author = document.querySelector('#author').value;
+    var price = document.querySelector('#price').value;
+
+    var newData = {
+      id: id,
+      title: title,
+      author: author,
+      price: price
+    };
+    newData.editable = false;
+
+    // event deligation
+    if (!e.target || e.target.nodeName !== 'I') return;
+    // 이벤트 걸어줄 대상이 되는 버튼
+    var buttonType = e.target.dataset.type;
+    if (buttonType === 'submit' && !newData.editable) {
+      // ADD -> submit: 첫 생성인 경우
+      newData.editable = true;
+      newData.status = 'new';
+      addItem(newData);
+      loadList();
     }
-  });
-  // 2-2. cancel을 누르는 경우
-  cancelBtn.addEventListener('click', function () {
-    // 첫 생성인 경우
-    if (!inputData.editable) {
-      // 해당 리스트 삭제
-    } else {// 수정인 경우
-        // 실행 취소
-      }
   });
 });
 
-// 3. 리스트의 아이템 편집(수정)
+// 3. edit  버튼을 눌렀을 때
+// editBtn.addEventListener('click', () => {
+//   tbody.addEventListener('click', (e) => {
+//     const buttonType = e.target.dataset.type;
+//     const id = e.target.dataset.item;
 
-// 4. 리스트의 아이템 삭제
+//     // 사용자가 입력한 input값
+//     const title = document.querySelector('#title').value;
+//     const author = document.querySelector('#author').value;
+//     const price = document.querySelector('#price').value;
 
-// tbody.addEventListener('click', (e) => {
-//   if (!e.target || e.target.nodeName !== 'BUTTON') return;
+//     const newData = {
+//       id,
+//       title,
+//       author,
+//       price
+//     };
+//     newData.editable = false;
 
-//   const tragetID = e.target.dataset.item * 1;
-
+//     // event deligation
+//     if (!e.target || e.target.nodeName !== 'I') return;
+//     // 버튼이 submit이고 수정인 경우
+//     if (buttonType === 'submit' && newData.editable) {
+//       newData.status = 'edited';
+//       updateItem(newData, id);
+//       loadList();
+//     }
+//   });
 // });
 
 /***/ }),
--- practice/simplebookList/src/js/main.js
+++ practice/simplebookList/src/js/main.js
@@ -5,10 +5,18 @@ const tbody = document.querySelector('tbody');
 
 // 버튼
 const addBtn = document.querySelector('#add-btn');
-const editBtn = document.querySelector('#del-btn');
+// const editBtn = document.querySelector('#del-btn');
+
+class BookList {
+  constructor() {
+    this.url = '/books';
+    this.books = [];
+  }
+}
 
 // =============================================
 // 함수
+
 // (1) bookList를 그려주는 기능
 function render(data) {
   // 리스트 초기화
@@ -44,102 +52,114 @@ function appendInput(id) {
     <td><input class="validate" type="text" id="author" placeholder="저자를 입력하세요."></td>
     <td><input class="validate" type="text" id="price" placeholder="가격을 입력하세요."></td>
     <td>  
-      <a class="waves-effect waves-gray btn-flat" id="submit-btn" data-item="${id}" data-type="submit">
-        <i class="medium material-icons">done</i>
+      <a class="waves-effect waves-gray btn-flat" id="submit-btn">
+        <i class="medium material-icons" data-item="${id}" data-type="submit">done</i>
       </a>
-      <a class="waves-effect waves-gray btn-flat" id="cancel-btn" data-item="${id}" data-type="cancel">
-        <i class="medium material-icons">cancel</i>
+      <a class="waves-effect waves-gray btn-flat" id="cancel-btn">
+        <i class="medium material-icons" data-item="${id}" data-type="cancel">cancel</i>
       </a>
-      <a class="waves-effect waves-gray btn-flat" id="del-btn" data-item="${id}" data-type="delete">
-        <i class="medium material-icons">delete</i>
+      <a class="waves-effect waves-gray btn-flat" id="del-btn">
+        <i class="medium material-icons"  data-item="${id}" data-type="delete">delete</i>
       </a>
     </td>
     </tr>`);
 }
 
 // (3) 새 아이템 추가 기능
-function addItem(inputData) {
-  // post해서 받은 데이터를 추가한다.
-  AJAX.post('/books', inputData).then((data) => {
-    render(JSON.parse(data));
-  });
+function addItem(newData) {
+  // console.log(newData);
+  // 아이템이 추가된 배열을 post로 보낸다.
+  AJAX.post('/books', JSON.stringify(newData));
 }
 
 // (4) 기존 아이템 업데이트 기능
-function updateItem(inputData, id) {
-  const path = '/books' + id;
-  AJAX.put(path, inputData).then((data) => {
-    render(JSON.parse(data));
-  });
+function updateItem(newData, id) {
+  const path = '/books/' + id;
+  AJAX.put(path, JSON.stringify(newData));
 }
 
 // =============================================
 // 화면
 // 1. 첫 페이지: 북리스트
-AJAX.get('/books').then((data) => {
-  // console.log(JSON.parse(data));
-  render(JSON.parse(data));
-});
 
-// 2. 리스트에 아이템(book) 추가
+// bookList 로드
+
+function loadList() {
+  AJAX.get('/books').then((data) => {
+    BookList.books = (JSON.parse(data));
+    // console.log(BookList.books);
+    render(BookList.books);
+  });
+}
+
+loadList();
+
+
+// 2. add버튼 눌렀을 때
 addBtn.addEventListener('click', () => {
   // 현재 리스트 아이템 갯수 = tr의 갯수
   const length = tbody.childElementCount;
   const id = length + 1;
+
+  // 리스트에 input 항목 추가
   appendInput(id);
-});
-
-tbody.addEventListener('click', (e) => {
-  if (!e.target || e.target.nodeName !== 'BUTTON') return;
-  // 이벤트 걸어줄 대상이 되는 버튼과 데이터 ID
-  const tragetID = e.target.dataset.item * 1;
-
-  // 클릭 이벤트에서만 생기는 버튼
-  const submitBtn = document.querySelector('#submit-btn');
-  const cancelBtn = document.querySelector('#cancel-btn');
-
-  // 사용자가 입력한 input값
-  const title = document.querySelector('#title');
-  const author = document.querySelector('#author');
-  const price = document.querySelector('#price');
-
-  const inputData = { title, author, price };
-  inputData.editable = false;
 
   // 2-1. submit을 누르는 경우
-  submitBtn.addEventListener('click', () => {
-    // 첫 생성인 경우
-    if (!inputData.editable) {
-      inputData.status = 'new';
-      inputData.editable = true;
-      // post
-      addItem(inputData);
-    } else { // 수정인 경우
-      inputData.status = 'edited';
-      // put
-      updateItem(inputData, tragetID);
-    }
-  });
-  // 2-2. cancel을 누르는 경우
-  cancelBtn.addEventListener('click', () => {
-    // 첫 생성인 경우
-    if (!inputData.editable) {
-      // 해당 리스트 삭제
-    } else { // 수정인 경우
-      // 실행 취소
+  // submitBtn.addEventListener('click', () => {
+  tbody.addEventListener('click', (e) => {
+    // 사용자가 입력한 input값
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const price = document.querySelector('#price').value;
+
+    const newData = {
+      id,
+      title,
+      author,
+      price
+    };
+    newData.editable = false;
+
+    // event deligation
+    if (!e.target || e.target.nodeName !== 'I') return;
+    // 이벤트 걸어줄 대상이 되는 버튼
+    const buttonType = e.target.dataset.type;
+    if (buttonType === 'submit' && !newData.editable) {
+      // ADD -> submit: 첫 생성인 경우
+      newData.editable = true;
+      newData.status = 'new';
+      addItem(newData);
+      loadList();
     }
   });
 });
 
-// 3. 리스트의 아이템 편집(수정)
+// 3. edit  버튼을 눌렀을 때
+// editBtn.addEventListener('click', () => {
+//   tbody.addEventListener('click', (e) => {
+//     const buttonType = e.target.dataset.type;
+//     const id = e.target.dataset.item;
 
-// 4. 리스트의 아이템 삭제
+//     // 사용자가 입력한 input값
+//     const title = document.querySelector('#title').value;
+//     const author = document.querySelector('#author').value;
+//     const price = document.querySelector('#price').value;
 
-// tbody.addEventListener('click', (e) => {
-//   if (!e.target || e.target.nodeName !== 'BUTTON') return;
+//     const newData = {
+//       id,
+//       title,
+//       author,
+//       price
+//     };
+//     newData.editable = false;
 
-//   const tragetID = e.target.dataset.item * 1;
-
+//     // event deligation
+//     if (!e.target || e.target.nodeName !== 'I') return;
+//     // 버튼이 submit이고 수정인 경우
+//     if (buttonType === 'submit' && newData.editable) {
+//       newData.status = 'edited';
+//       updateItem(newData, id);
+//       loadList();
+//     }
+//   });
 // });
-
-
