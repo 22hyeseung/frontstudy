2b3c72bc9562ab2487f9b1809995c52a1d1845e0
huusz <irinso93@gmail.com>
2017-09-24 18:13:32 +0900

    Provider 컴포넌트 임포트 & store 연동

--- React/Redux/practice/redux-counter/src/index.js
+++ React/Redux/practice/redux-counter/src/index.js
@@ -6,8 +6,14 @@ import './index.css';
 // Redux 관련 불러오기
 import { createStore } from 'redux';
 import reducer from './reducers';
+import { Provider } from 'react-redux';
 
 // 스토어 생성
 const store = createStore(reducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
