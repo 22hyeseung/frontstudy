9f3d697684c709a299101dc044ee626b3c5212ef
huusz <irinso93@gmail.com>
2018-01-07 22:01:25 +0900

    modify node test code

--- TDD/nodejs/index.js
+++ TDD/nodejs/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const users = [{ id: 1, name: 'a' }, { id: 2, name: 'b' }];
+const users = [
+  { id: 1, name: 'a' },
+  { id: 2, name: 'b' },
+  { id: 3, name: 'c' },
+];
 
 app.use(morgan('dev'));
 
 app.get('/users', (req, res) => {
-  res.json(users);
+  const limit = req.query.limit;
+  res.json(users.slice(0, limit));
 });
 
 app.listen(8000, () => {
