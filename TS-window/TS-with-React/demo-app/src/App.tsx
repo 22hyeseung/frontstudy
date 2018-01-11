5b3c96bbdcbd85f6bf4b081bcd561380b5e090f4
huusz <irinso93@gmail.com>
2018-01-11 15:19:40 +0900

    Delete default app code and Add demo code

--- TS-window/TS-with-React/demo-app/src/App.tsx
+++ TS-window/TS-with-React/demo-app/src/App.tsx
@@ -1,19 +1,12 @@
 import * as React from 'react';
+import Hello from './components/Hello';
 import './App.css';
-
-const logo = require('./logo.svg');
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Hello name="Huusz" enthusiasmLevel={10} />
       </div>
     );
   }
